d968e4849506aff4e4d952cc71e7226373b1478f
Lyaskin Boris <lyaskinboris@yandex.ru>
2020-07-07 23:15:04 +0700

    added mask to datepicker

--- src/app/shared/controls/controls.module.ts
+++ src/app/shared/controls/controls.module.ts
@@ -1,16 +1,17 @@
-import { CtrlInputComponent } from './ctrl-input/ctrl-input.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { NgxMaskModule } from 'ngx-mask';
 
 
 import { CtrlAutocompleteAddressComponent } from './ctrl-autocomplete-address/ctrl-autocomplete-address.component';
 import { CtrlDatepickerComponent } from './ctrl-datepicker/ctrl-datepicker.component';
+import { CtrlInputComponent } from './ctrl-input/ctrl-input.component';
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { CommonModule } from '@angular/common';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    NgxMaskModule.forRoot(),
   ]
 })
 export class ControlsModule {
